694ff745f895f63c906df8479a0d3877a339b617
Mohammed Elghanam <144786058+MohammedElghanam@users.noreply.github.com>
2024-12-18 16:43:59 +0100

    feat: remove part of code

--- api_getway/src/auth/auth.service.ts
+++ api_getway/src/auth/auth.service.ts
@@ -1,6 +1,4 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
 import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
@@ -9,26 +7,6 @@ export class AuthService {
   constructor(
     @Inject('SERVICE_1') private readonly serviceAClient: ClientProxy,
   ) {}
-
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
-
-  findAll() {
-    return `This action returns all auth`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
-
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
-  }
 
   async login(body: any) {
     const result = await this.serviceAClient.send({ cmd: 'login' }, body).toPromise();
